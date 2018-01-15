a6d23504667afcbb1f84ae6cfe2bc2a3bb457a06
Anuradha Weeraman <anuradha@weeraman.com>
2018-01-15 21:53:11 +0530

    Initial commit

---
+++ request-repository-service/src/main/java/life/givesomeone/requestrepositoryservice/RequestRepositoryServiceApplication.java
@@ -0,0 +1,12 @@
+package life.givesomeone.requestrepositoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequestRepositoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RequestRepositoryServiceApplication.class, args);
+	}
+}
